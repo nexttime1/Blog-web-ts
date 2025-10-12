fb0b0742c7559a68816f9eef9733629e67f7719a
xtm <2083933996@qq.com>
2025-10-12 21:37:37 +0800

    feat-10/12

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from "node:url";
+
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import type { ImportMetaEnv } from "./env";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   let env: Record<keyof ImportMetaEnv, string> = loadEnv(mode, process.cwd());
-  console.log(env.VITE_SERVER_URL, process.cwd());
+
+  const serverUrl = env.VITE_SERVER_URL;
+  const wsUrl = serverUrl.replace("http", "ws");
   return {
     plugins: [vue()],
     envDir: "./",
@@ -20,12 +22,18 @@ export default defineConfig(({ mode }) => {
       port: 80,
       proxy: {
         "/api": {
-          target: env.VITE_SERVER_URL,
+          target: serverUrl,
           changeOrigin: true,
         },
         "/uploads": {
-          target: env.VITE_SERVER_URL,
+          target: serverUrl,
           changeOrigin: true,
+        },
+        "/ws": {
+          target: wsUrl,
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/ws/, ""),
         },
       },
     },
